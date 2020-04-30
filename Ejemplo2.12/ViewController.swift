47d068472cdf4f1e683e0dd219dbbe7d2507d649
David Camilo Baquero <dcbgper@gmail.com>
2020-04-30 09:20:36 -0500

    Initial Commit

---
+++ Ejemplo2.12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejemplo2.12
+//
+//  Created by David Camilo Baquero on 4/30/20.
+//  Copyright © 2020 nextu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
